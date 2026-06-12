1188527d24fa369c0046cd012f9b51f61a8c7f07
Temitope <olawunitemitope68@gmail.com>
2026-06-12 09:44:19 +0100

    matches-endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { db } from "./db/db.js";
 import { demoUsers } from "./db/schema.js";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
 const port = 8000;
@@ -8,35 +9,10 @@ const port = 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express!" });
+  res.send({ message: "Hello from Express!" });
 });
 
-app.get("/users", async (req, res) => {
-  try {
-    const users = await db.select().from(demoUsers);
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch users" });
-  }
-});
-
-app.post("/users", async (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
-    return res.status(400).json({ error: "Name is required" });
-  }
-
-  try {
-    const inserted = await db.insert(demoUsers).values({ name }).returning();
-    res.status(201).json(inserted);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to create user" });
-  }
-});
-
+app.use('/matches', matchRouter)
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
